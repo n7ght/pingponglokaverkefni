295239c3612d26d1ef51a8eabd4161bfcf578a6f
n7ght <kristinnthor@kvikmyndaskoli.is>
2016-11-22 17:13:59 +0000

    kiddi push 3

--- pplokav/pplokav/Form1.Designer.cs
+++ pplokav/pplokav/Form1.Designer.cs
@@ -29,26 +29,38 @@
         private void InitializeComponent()
         {
             this.kiddipannel = new System.Windows.Forms.Panel();
+            this.spaði = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.kiddipannel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.spaði)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // kiddipannel
             // 
             this.kiddipannel.Controls.Add(this.pictureBox1);
+            this.kiddipannel.Controls.Add(this.spaði);
             this.kiddipannel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.kiddipannel.Location = new System.Drawing.Point(0, 0);
             this.kiddipannel.Name = "kiddipannel";
             this.kiddipannel.Size = new System.Drawing.Size(875, 480);
             this.kiddipannel.TabIndex = 0;
             // 
+            // spaði
+            // 
+            this.spaði.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.spaði.Location = new System.Drawing.Point(322, 434);
+            this.spaði.Name = "spaði";
+            this.spaði.Size = new System.Drawing.Size(200, 20);
+            this.spaði.TabIndex = 0;
+            this.spaði.TabStop = false;
+            // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(330, 235);
+            this.pictureBox1.Location = new System.Drawing.Point(349, 161);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
             // Form1
@@ -60,6 +72,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.kiddipannel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.spaði)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -68,6 +81,7 @@
         #endregion
 
         private System.Windows.Forms.Panel kiddipannel;
+        private System.Windows.Forms.PictureBox spaði;
         private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
