953d2df5afcc085bdbcaa4a078f05dc4aae5e5b0
gudmundur98 <gummibr98@gmail.com>
2016-11-22 17:18:06 +0000

    variables komin inn

--- pplokav/pplokav/Form1.cs
+++ pplokav/pplokav/Form1.cs
@@ -12,6 +12,11 @@ namespace pplokav
 {
     public partial class Form1 : Form
     {
+
+        public int hradi_vinstri = 4;
+        public int hradi_efst = 4;
+        public int stig = 0;
+
         public Form1()
         {
             InitializeComponent();
